b364cae6c229f1c966e8a6e4adc67805ab38e43d
Lenny ROUANET <lennyrouanet@users.noreply.github.com>
2022-09-20 13:43:09 +0200

    Test

--- test/Domain/DataStructure/Value/ApiKeyTest.php
+++ test/Domain/DataStructure/Value/ApiKeyTest.php
@@ -14,4 +14,22 @@ final class ApiKeyTest extends \PHPUnit\Framework\TestCase
 		$this->assertIsObject($value);
 		$this->assertInstanceOf(ApiKey::class, $value);
 	}
+	
+	public function testCheck(): void
+	{
+		$value = ApiKey::generate();
+		$result = $value->check($value);
+		
+		$this->assertIsBool($result);
+		$this->assertEquals(true, $result);
+	}
+	
+	public function testCheckInvalid(): void
+	{
+		$value = ApiKey::generate();
+		$result = $value->check(ApiKey::generate());
+		
+		$this->assertIsBool($result);
+		$this->assertEquals(false, $result);
+	}
 }
